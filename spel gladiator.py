b8877d6ecb9ed590bbdb96c094ee93d761322c48
Simon Odding <simon.odding@learnet.se>
2024-10-10 13:03:02 +0200

    l

---
+++ spel gladiator.py
@@ -0,0 +1,6 @@
+import colorama, msvcrt, random
+print(for,blue, "hej")
+
+print ("Du är gladiator maximus, nu ska du slåss mot gladiatorn wilmer. platsen ni befinner er på är en romersk 
+gladiator arena omgivna av en förväntansfull publik. Ni har inga vapen eller utrustning utan du är klädd enbart i ett par långa läderbyxor
+, ett par läderskor och armand gjorde av stål. Din bara bronsfärgade bringa lyses")   
